3484e928dcd93bda0990d11d44954371c95c3b3a
angelGarciaSantos <angel.garcia.90@gmail.com>
2021-05-08 18:24:31 +0200

    Implemented posts CRUD

---
+++ controllers/post.js
@@ -0,0 +1,74 @@
+'use strict'
+
+const Post = require('../models/post')
+
+function getPost (req, res) {
+    let postId = req.params.postId
+
+    Post.findById(postId, (err, post) => {
+        if (err)  return  res.status(500).send({message: `Error al realizar la petición: ${err}`})
+        if (!post) return res.status(404).send({message: `El tema no existe`})
+
+        return res.status(200).send({ post: post })
+    })
+}
+
+function getPosts (req, res) {
+    Post.find({}, (err, posts) =>{
+        if (err)  return  res.status(500).send({message: `Error al realizar la petición: ${err}`})
+        if (!posts) return res.status(404).send({message: `No existen temas`})
+
+        return res.send(200, {posts: posts})
+    })
+}
+
+function savePost (req, res) {
+    console.log('POST /api/post')
+    console.log(req.body)
+
+    let date = new Date()
+
+    let post = new Post()
+    post.title = req.body.title
+    post.content = req.body.content
+    post.user = req.body.user
+    post.date = date
+
+    post.save((err, postStored) => {
+        if(err) res.status(500).send({message: `Error al salvar en la base de datos: ${err}`})
+
+        res.status(200).send({post: postStored})
+    })
+
+}
+
+function updatePost (req, res) {
+    let postId = req.params.postId
+    let update = req.body
+
+    Post.findByIdAndUpdate(postId, update, (err, postUpdated) => {
+        if (err)  return  res.status(500).send({message: `Error al actualizar el tema: ${err}`})
+        res.status(200).send({ post: postUpdated })
+    })
+}
+
+function deletePost (req, res) {
+    let postId = req.params.postId
+
+    Post.findById(postId, (err, post) => {
+        if (err)  return  res.status(500).send({message: `Error al borrar el tema: ${err}`})
+        
+        post.remove(err => {
+            if (err)  return  res.status(500).send({message: `Error al borrar el tema: ${err}`})
+            res.status(200).send({ message: 'El producto ha sido eliminado' })
+        })
+    })
+}
+
+module.exports = {
+    getPost,
+    getPosts,
+    savePost,
+    updatePost,
+    deletePost
+}
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,29 @@
 const express = require('express')
 const productCtrl = require('../controllers/product')
 const userCtrl = require('../controllers/user')
+const postCtrl = require('../controllers/post')
 const auth = require('../middlewares/auth')
 const api = express.Router()
 
+// Products
 api.get('/product', auth, productCtrl.getProducts)
 api.get('/product/:productId', productCtrl.getProduct)
 api.post('/product', auth, productCtrl.saveProduct)
 api.put('/product/:productId', auth, productCtrl.updateProduct)
 api.delete('/product/:productId', auth, productCtrl.deleteProduct)
 
+// Posts
+api.get('/post', postCtrl.getPosts)
+api.get('/post/:postId', postCtrl.getPost)
+api.post('/post', postCtrl.savePost)
+api.put('/post/:postId', postCtrl.updatePost)
+api.delete('/post/:postId', postCtrl.deletePost)
+
+// Sign in / Sign up
 api.post('/signup', userCtrl.signUp)
 api.post('/signin', userCtrl.signIn)
 
-
-
+// Private resource test (auth in middle)
 api.get('/private', auth, function (req, res) {
     res.status(200).send({message: 'Tienes acceso'})
 })
