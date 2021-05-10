c6611649bff2b4a244781ea9eb4477281eb81174
angelGarciaSantos <angel.garcia.90@gmail.com>
2021-05-10 09:56:38 +0200

    Asking for auth in create, update and delete posts

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,9 @@ api.delete('/product/:productId', auth, productCtrl.deleteProduct)
 // Posts
 api.get('/post', postCtrl.getPosts)
 api.get('/post/:postId', postCtrl.getPost)
-api.post('/post', postCtrl.savePost)
-api.put('/post/:postId', postCtrl.updatePost)
-api.delete('/post/:postId', postCtrl.deletePost)
+api.post('/post', auth, postCtrl.savePost)
+api.put('/post/:postId', auth, postCtrl.updatePost)
+api.delete('/post/:postId', auth, postCtrl.deletePost)
 
 // Sign in / Sign up
 api.post('/signup', userCtrl.signUp)
